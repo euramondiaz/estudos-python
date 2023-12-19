4f9bdb1fdd42d1fcd10847af2b99c435997d4c78
Ramon Dias <ramondiaz.ads@gmail.com>
2023-12-19 16:15:02 -0300

    new update

---
+++ variavaies e tipos de dados/variaveis.py
@@ -0,0 +1,36 @@
+Nome_usuario = 'Ramon Diaz'
+print(Nome_usuario)
+
+media = 0   #int
+n1 = n2 = n3 = n4 = 0.0 #float
+nome, idade = 'Ramon', 25 #string
+estado = True #bool
+
+
+# Função type(), para saber o timo de variavel
+print (type(media))
+print (type(n2))
+print (type(nome))
+print (type(estado))
+print (type(1+2j)) #complex
+
+
+
+# função isinstance() diz se a variavel especificado esta correta ou não
+a = 25
+b = 'casa'
+print(isinstance(a,int))
+
+# # A função `isinstance()` é uma maneira de verificar 
+# se um objeto é uma instância de uma classe.
+#
+# Ela é útil para verificar se um objeto tem as características esperadas de uma classe.
+#
+# Por exemplo, se você tiver uma classe `Animal` com 
+#um método `latir()`, você pode usar a função `isinstance()` 
+#para verificar se um objeto é uma instância de `Animal` antes de 
+#chamar o método `latir()`.
+
+
+
+#codigo rodando paraa testes no terminal
