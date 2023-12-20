d6c3ffc5e79e8cd93dbf7b786220f6b98372d796
Ramon Dias <ramondiaz.ads@gmail.com>
2023-12-20 15:17:49 -0300

    new file

---
+++ operadores aritmeticos/operadores-a.py
@@ -0,0 +1,17 @@
+ # x = y= z = 0
+
+# x = 7
+# y = 9
+
+# z = x + y
+
+# print ('A soma dos dois valores é', z)
+
+x = y = z = 0
+
+x = int(input('Digite um numero'))
+y = int(input('digite outro numero'))
+
+z = x + y
+
+print('a soma dos valores é', z)
