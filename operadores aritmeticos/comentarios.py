d6f1355d7d889dcdbe2924f24b3473b2113918e6
Ramon Dias <ramondiaz.ads@gmail.com>
2024-01-02 14:30:11 -0300

    operadores comparação

---
+++ operadores aritmeticos/comentarios.py
@@ -0,0 +1,23 @@
+# Os operadores aritméticos são usados para realizar cálculos matemáticos em Python.
+# Os operadores aritméticos básicos são:
+#
+# * **+** - Adição
+# * **-** - Subtração
+# * **\** - Multiplicação
+# * **/** - Divisão
+#
+# Por exemplo, a seguinte expressão soma dois números:
+#
+# ```python
+# a = 1 + 2
+# ```
+#
+# O resultado desta expressão é 3.
+#
+# Outros operadores aritméticos incluem:
+#
+# * **%** - Resto da divisão
+# * **** - Exponenciação
+# * **//** - Divisão inteira
+
+
