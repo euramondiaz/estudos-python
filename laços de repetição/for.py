80c9f8e2c4b29823e76588272fa4b55d4cda781b
Ramon Dias <ramondiaz.ads@gmail.com>
2024-01-05 17:33:44 -0300

    add update

---
+++ laços de repetição/for.py
@@ -0,0 +1,15 @@
+# lista = [2,6,9,4,0,12,3,7]
+# palavra = 'Ramon'
+# for letra in palavra:
+#     print(letra)
+
+
+
+######função RANGE#########
+# for numero in range(1,11):
+#     print( numero)
+
+
+nome=input('digite seu nome:')
+for x in range (10):
+    print(f'{x+1} {nome}')
