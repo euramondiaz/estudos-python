a8dc060e90b8ce601554b96b8627434a81e154fe
Ramon Dias <ramondiaz.ads@gmail.com>
2024-01-23 10:03:07 -0300

    update numero aleatorios

---
+++ numero aleatorios/aleatorios.py
@@ -0,0 +1,4 @@
+import random
+
+valor = random.randint(1,20)
+print(valor)
---
+++ numero aleatorios/random+for.py
@@ -0,0 +1,6 @@
+import random
+
+print('gerar cinco numeros elatorios entre 1 e 50, \n')
+for i in range(5):
+    n = random.randint(1,50)
+    print(f'numero gerado: {n}')
