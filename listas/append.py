0245ec0155f8084fa22129fdd7eb13af9925d208
Ramon Dias <ramondiaz.ads@gmail.com>
2024-01-23 13:19:28 -0300

    update comments append

--- listas/append.py
+++ listas/append.py
@@ -1,8 +1,13 @@
+# Duas listas são definidas, n1 e n2, contendo valores numéricos.
 n1 = [4, 6, 7, 8, 0, 3]
 n2 = [1, 6, 3, 0, 12, 11]
+
+# A lista 'valores' é criada concatenando as listas n1 e n2.
 valores = n1 + n2
+
+# O primeiro elemento da lista 'valores' é alterado para 9.
 valores[0] = 9
 
+# Adiciona o valor 13 ao final da lista 'valores' utilizando o método append().
 valores.append(13)
 print(valores)
-
