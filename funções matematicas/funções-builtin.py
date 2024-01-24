93e6fd40c950da4347d221e4a7d05bd6b7a1a080
Ramon Dias <ramondiaz.ads@gmail.com>
2024-01-24 19:37:39 -0300

    first builtin

---
+++ funções matematicas/funções-builtin.py
@@ -0,0 +1,24 @@
+# A lista 'valores' é criada contendo valores numéricos.
+valores = [2, 3, 42, 5, -1, 2, 4, 0, -6]
+
+# Imprime o valor máximo da lista 'valores' utilizando a função max().
+print(max(valores))
+
+# Imprime o valor mínimo da lista 'valores' utilizando a função min().
+print(min(valores))
+
+# As variáveis 'a' e 'b' recebem valores numéricos.
+a = -5
+b = 4
+
+# Imprime o valor absoluto de 'a' utilizando a função abs().
+print(abs(a))
+
+# Imprime 'a' elevado à potência 'b' utilizando a função pow().
+print(pow(a, b))
+
+# A variável 'c' recebe um valor decimal.
+c = 2.79384545
+
+# Imprime 'c' arredondado para 6 casas decimais utilizando a função round().
+print(round(c, 6))
